148f2d7da28ada74924bbf4338d21f7ab9874046
Guilheme do C. Costa <gguillhherrmmecosta@gmail.com>
2021-09-04 09:13:28 -0300

    configurando espaço em JavaScript

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function test() {
   var hora = data.getHours()
   msg.innerHTML = `Agora são ${hora} horas.`
   if (hora >= 0 && hora <12) {
-      msg.innerHTML = `Hi &#x1f590 Bom dia! Agora são${hora} horas.`
+      msg.innerHTML = `Hi &#x1f590 Bom dia! Agora são ${hora} horas.`
   }else if(hora >= 12 && hora <18) {
       msg.innerHTML = `Hi &#x1f590 Boa tarde! Agora são ${hora} horas.`
   } else {
