5badc1524642d937bf7f0c2374827ce6da27b2d5
Guilherme-Costa <gguillhherrmmecosta@gmail.com>
2021-09-04 10:00:04 -0300

    editando arquivo JS

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function test() {
   var hora = data.getHours()
   msg.innerHTML = `Agora são ${hora} horas.`
   if (hora >= 0 && hora <12) {
-      msg.innerHTML = `Hi🖐 Bom dia! Agora são${hora} horas.`
+      msg.innerHTML = `Hi🖐 Bom dia! Agora são ${hora} horas.`
   }else if(hora >= 12 && hora <18) {
       msg.innerHTML = `Hi🖐 Boa tarde! Agora são ${hora} horas.`
   } else {
